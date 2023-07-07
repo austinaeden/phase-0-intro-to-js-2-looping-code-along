995b4d4964ff0ccab9e4fd0deaa376cdabbd9dca
austinaeden <austin.mbogo@student.moringaschool.com>
2023-07-07 09:09:39 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solutions in this file
+const thankYouMessages = [];
+
+function writeCards(names, ocassion) {
+
+    for (let i = 0; i < names.length; i++){
+        const messages = `Thank you, ${names[i]}, for the wonderful ${ocassion} gift!`;
+        
+        thankYouMessages.push(messages);
+    }
+    return thankYouMessages;
+}
+
+function countDown(number){
+    let countDown = 10;
+    while (countDown >= 0) {
+        console.log(countDown--);
+    }
+}
+
+//the opposite for counting up 
+//let countup = 0;
+//while (countup < 10) {
+//  console.log(countup++);
+//}
